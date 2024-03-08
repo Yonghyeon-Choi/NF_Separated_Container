03230159945cf879541f1dc9862aff7520805111
Yonghyeon-Choi <choiyh0601@naver.com>
2024-03-08 09:56:21 +0900

    iptables persistent

---
+++ coreimages/5_upf/tunup-set.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if ! grep "ogstun" /proc/net/dev > /dev/null; then
+    ip tuntap add name ogstun mode tun
+fi 
+ip addr del 10.45.0.1/16 dev ogstun 2> /dev/null
+ip addr add 10.45.0.1/16 dev ogstun
+ip addr del 2001:db8:cafe::1/48 dev ogstun 2> /dev/null
+ip addr add 2001:db8:cafe::1/48 dev ogstun
+ip link set ogstun up
+
+sysctl -w net.ipv4.ip_forward=1
+sysctl -w net.ipv6.conf.all.forwarding=1
+
+#iptables -t nat -A POSTROUTING -s 10.45.0.0/16 ! -o ogstun -j MASQUERADE
+#ip6tables -t nat -A POSTROUTING -s 2001:db8:cafe::/48 ! -o ogstun -j MASQUERADE
+
+## Prevent UE from connecting to UPF host
+#iptables -I INPUT -i ogstun -j ACCEPT
+#iptables -I INPUT -s 10.45.0.0/16 -j DROP
+
+#echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
+#echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
+#apt-get -y install iptables-persistent
+
+systemctl restart open5gs-upfd
--- k8src/iptable-persistent.sh
+++
@@ -1,4 +0,0 @@
-./iptables.sh &&\
-iptables-persistent iptables-persistent/autosave_v4 boolean false | debconf-set-selections &&\
-iptables-persistent iptables-persistent/autosave_v4 boolean false | debconf-set-selections &&\
-apt-get -y install iptables-persistent
